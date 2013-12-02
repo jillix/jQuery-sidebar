b89b9be9415b0630481d9ef03589079f04c510c6
Ionică Bizău <bizauionica@yahoo.com>
2013-12-02 19:22:02 +0200

    Added the plugin script.

---
+++ jquery.sidebar.js
@@ -0,0 +1,67 @@
+/*
+ *  jQuery sidebar plugin
+ *  ---------------------
+ *  A very simple sidebar jQuery plugin
+ *
+ *  Copyright (c) 2013 - jillix gmbh
+ *
+ * */
+;(function ($){
+
+    /*
+     *  $("[jQuery selector]").sidebar({...});
+     * */
+    $.fn.sidebar = function(options) {
+
+        // defaults
+        var settings = $.extend( {
+            speed         : 200,         // animate speed
+            side          : 'left'       // side: 'left' or 'right'
+        }, options);
+       
+        // get 'this' jQuery element
+        var self = this;
+       
+        /*
+         *  Opens the sidebar
+         *  $("[jQuery selector]).trigger("open");
+         * */
+        this.on("open", function () {
+
+            // animate properties
+            var properties = {};
+
+            // animate to 0px the `right/left`
+            properties[settings.side] = 0;
+           
+            // start animation
+            self.animate(properties, settings.speed, function () {
+                // finally emit something
+                self.trigger("opened");
+            });
+        });
+       
+
+        /*
+         *  Closes the sidebar
+         *
+         *  $("[jQuery selector]).trigger("close");
+         * */
+        this.on("close", function (callback) {
+            
+            // animate properties
+            var properties = {};
+
+            // TODO padding? borders?
+            properties[settings.side] = - self.width() - 30;
+
+            // start animation
+            self.animate(properties, settings.speed, function () {
+                // finally emit something
+                self.trigger("closed");
+            });
+        });
+       
+        return this;
+    };
+})(jQuery);
