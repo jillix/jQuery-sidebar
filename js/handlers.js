0107176a3f2f44e1c56afbd0a86a91b65bfa24de
Ionică Bizău <bizauionica@yahoo.com>
2014-12-26 11:50:49 +0200

    New demo page

---
+++ js/handlers.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // All sides
+    var sides = ["left", "top", "right", "bottom"];
+    $("h1 span.version").text($.fn.sidebar.version);
+
+    // Initialize sidebars
+    for (var i = 0; i < sides.length; ++i) {
+        var cSide = sides[i];
+        $(".sidebar." + cSide).sidebar({side: cSide});
+    }
+
+    // Click handlers
+    $(".btn[data-action]").on("click", function () {
+        var $this = $(this);
+        var action = $this.attr("data-action");
+        var side = $this.attr("data-side");
+        $(".sidebar." + side).trigger("sidebar:" + action);
+        return false;
+    });
+});
